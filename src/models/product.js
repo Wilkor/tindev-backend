ad3ac279dceb6184f9255f19a7faa7bbe525bf2c
wilkor <wilkorguitarrista@msn.com>
2020-07-11 21:09:28 -0300

    criando tabelas iniciais

---
+++ src/models/product.js
@@ -0,0 +1,31 @@
+const {Schema,model} = require("mongoose");
+
+const ProductSchema = new Schema({
+
+  productName:{
+    type:String,
+    required:true
+  },
+  user:{
+    type:String,
+    required:true
+  },
+  urlFireBase:{
+    type:String,
+    required:true
+  },
+  image:{
+    type:String,
+    required:true
+  },
+  category:{
+    type:String,
+    required:true
+  }
+
+},
+{
+    timestamps:true
+  });
+
+  module.exports = model('Product',ProductSchema);
