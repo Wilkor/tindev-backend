1a11b8b1d3ac22de093563764b99f1e440f11e0b
wilkor <wilkorguitarrista@msn.com>
2020-07-14 20:18:44 -0300

    ordenando produtos

--- src/controllers/DislikeController.js
+++ src/controllers/DislikeController.js
@@ -4,26 +4,24 @@ module.exports = {
 
    async store(req,res){
 
-    const loggedDev =  await Dev.findOne(req.headers.user);
-    const targetDev = await Dev.findOnde(req.params.devId);
+    const loggedDev =  await Dev.findById(req.headers.user);
+   //const targetDev = await Dev.findOnde(req.params.devId);
 
     if(!targetDev){
         return res.status(404).json({error:'Dev not exists'})
     }
 
 
-  
-    const filter2 = { user: req.params.devId};
+    const filter = { user: req.headers.user };
     const update = { hasLike: true };
     
-    await Product.findOneAndUpdate(filter2, update, {
-     new: true
-   });
+     await Product.findByIdAndUpdate(filter, update, {
+      new: true
+    });
 
+      loggedDev.dislikes.push(targetDev._id)
 
-    loggedDev.dislikes.push(targetDev._id)
-
-    await loggedDev.save();
+      await loggedDev.save();
 
    return res.json({loggedDev})
    }
