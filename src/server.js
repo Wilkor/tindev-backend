c07a74ae3f56e667136c04227f634968f7f12347
wilkor <wilkorguitarrista@msn.com>
2020-07-16 02:23:04 -0300

    adicionando logou e atualizando o status do usuário

--- src/server.js
+++ src/server.js
@@ -49,7 +49,7 @@ io.on('connection',socket => {
     socket.on('disconnect', () => {
 
       const user = removeUser(socket.id);
-      const userExists = await Dev.findById(socket.handshake.query.user);
+      const userExists =  Dev.findById(socket.handshake.query.user);
 
       if(userExists){
         userExists.online = fasle;
