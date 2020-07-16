a86e9e13d83e306cda8a27dd374aade65664200a
wilkor <wilkorguitarrista@msn.com>
2020-07-16 01:44:20 -0300

    adicionando chat no projeto

--- src/controllers/NotificationController.js
+++ src/controllers/NotificationController.js
@@ -2,10 +2,17 @@ const Notification = require("../models/notification");
 
 module.exports = {
 
-   async index(req,res){
+   async index(req,res) {
 
     const notification =  await Notification.findOne({user:req.params.id});
 
-    return res.json({notification})
+    if(!notification) {
+
+       return res.json({notification});
+
+    }
+
+    return res.json([]);
+
    }
 }
